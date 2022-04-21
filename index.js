0938f7ed8e2adce693946540caa8c6383d4d7ddd
Calvin <cl.kleijn@student.avans.nl>
2022-04-21 17:46:37 +0200

    Fixed user delete request and added user put request

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Default settings
-const http = require('http')
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const http = require('http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Use body-parser
-const bodyParser = require("body-parser")
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-// Create a database array with an id
+// Create a database array and sort it on id
 let database = 
 [
     { 
@@ -33,9 +33,11 @@ let database =
         password: 'public',
         phoneNumber: '06 87654321'  
     },
-]
+].sort(function (x, y) {
+    return x.id - y.id
+});
 
-let id = 1
+let id = 1;
 
 // All user routes
 app.route('/user')
@@ -44,126 +46,142 @@ app.route('/user')
             res.status(200).json({
                 status: 200,
                 result: database
-            })
+            });
         } else {
             res.status(401).json({
                 status: 401,
                 result: 'Forbidden'
-            })
+            });
         }
-        res.end()
+        res.end();
     })
     .post((req, res) => {
-        let user = req.body
-        id++
-
+        let user = req.body;
+        id++;
         user = {
             id,
             ...user
         }
-
-        database.push(user)
-
+        database.push(user);
         res.status(201).json({
             status: 201,
             result: database
-        })
-        res.end()
-    })
+        });
+        res.end();
+    });
 
 app.get('/user/profile', (req, res) => {
     if(res.statusCode >= 200 && res.statusCode <= 299) {
-        let user = database.filter((item) => item.id == 0)
+        let user = database.filter((item) => item.id == 0);
         if(user.length > 0) {
             res.status(200).json({
                 status: 200,
                 result: user
-            })
+            });
         } else {
             res.status(401).json({
                 status: 401,
                 result: 'Forbidden'
-            })
+            });
         }
     } else {
         res.status(401).json({
             status: 401,
             result: 'Forbidden'
-        })
+        });
     }
-    res.end()
-})
+    res.end();
+});
 
 app.route('/user/:userId')
     .get((req, res) => {
         if(res.statusCode >= 200 && res.statusCode <= 299) {
-            const userId = req.params.userId
-            let user = database.filter((item) => item.id == userId)
+            const userId = req.params.userId;
+            let user = database.filter((item) => item.id == userId);
             if(user.length > 0) {
                 res.status(200).json({
                     status: 200,
                     result: user
-                })
+                });
             } else {
                 res.status(401).json({
                     status: 401,
                     result: 'Forbidden'
-                })
+                });
             }
         } else {
             res.status(401).json({
                 status: 401,
                 result: 'Forbidden'
-            })
+            });
         }
-        res.end()
+        res.end();
     })
     .put((req, res) => {
         if(res.statusCode >= 200 && res.statusCode <= 299) {
-            console.log('Got a PUT request at /user/:userId')
-            res.send('Got a PUT request at /user/:userId')
-        } else {
-            res.status(401).json({
-                status: 401,
-                result: 'Forbidden'
-            })
-        }
-        res.end()
-    })
-    .delete((req, res) => {
-        if(res.statusCode >= 200 && res.statusCode <= 299) {
             const userId = req.params.userId
-            let user = database.filter((item) => item.id == userId)
+            let user = database.filter((item) => item.id == userId);
+            let id = parseInt(userId);
             if(user.length > 0) {
-                database.splice(userId)
-                res.status(200).json({
-                    status: 200,
+                let updatedUser = req.body;
+                updatedUser = {
+                    id,
+                    ...updatedUser
+                }
+                database[userId] = updatedUser;
+                res.status(201).json({
+                    status: 201,
                     result: database
-                })
+                });
             } else {
                 res.status(401).json({
                     status: 401,
                     result: 'Forbidden'
-                })
+                });
             }
         } else {
             res.status(401).json({
                 status: 401,
                 result: 'Forbidden'
-            })
+            });
         }
-        res.end()
+        res.end();
     })
+    .delete((req, res) => {
+        if(res.statusCode >= 200 && res.statusCode <= 299) {
+            const userId = req.params.userId;
+            let user = database.filter((item) => item.id == userId);
+            if(user.length > 0) {
+                database.splice(userId, 1);
+                res.status(201).json({
+                    status: 201,
+                    result: database
+                });
+            } else {
+                res.status(401).json({
+                    status: 401,
+                    result: 'Forbidden'
+                });
+            }
+        } else {
+            res.status(401).json({
+                status: 401,
+                result: 'Forbidden'
+            });
+        }
+        res.end();
+    });
 
 // Return a 404 status if the link doesn't exist
 app.all('*', (req, res) => {
     res.status(404).json({
         status: 404,
         result: 'End-point not found'
-    })
-})
+    });
+    res.end();
+});
 
 // Show which port is set available for the app
 app.listen(port, () => {
-    console.log('App listening on port ' + port)
+    console.log('App listening on localhost:' + port)
 })
