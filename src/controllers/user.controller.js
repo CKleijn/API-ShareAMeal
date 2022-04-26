9ac09465007e29791f3996854b91506209698544
Calvin <cl.kleijn@student.avans.nl>
2022-04-26 14:07:20 +0200

    Refactor the routes into user.routes and put the functionality into user.controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,205 @@
+// Create a database array and sort it on id
+let database = 
+[
+    { 
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        street: 'Lovensdijkstraat 61',
+        city: 'Breda',
+        isActive: true,
+        emailAdress: 'john.doe@server.com',
+        password: 'secret',
+        phoneNumber: '06 12425475'  
+    },
+    { 
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        street: 'Hogeschoollaan 32',
+        city: 'Breda',
+        isActive: true,
+        emailAdress: 'jane.doe@server.com',
+        password: 'public',
+        phoneNumber: '06 87654321'  
+    },
+].sort(function (x, y) {
+    return x.id - y.id
+});
+
+let id = 2;
+
+let userController = {
+    getAllUsers: (req, res) => {
+        if(res.statusCode >= 200 && res.statusCode <= 299) {
+            res.status(200).json({
+                status: 200,
+                result: database
+            });
+        } else {
+            res.status(401).json({
+                status: 401,
+                result: 'Forbidden'
+            });
+        }
+        res.end();
+    },
+    addUser: (req, res) => {
+        if(res.statusCode >= 200 && res.statusCode <= 299) {
+            let user = req.body;
+            let isUsed = false;
+            database.forEach(item => {
+                if(item.emailAdress == user.emailAdress) {
+                    isUsed = true;
+                }
+            });
+            if(!isUsed) {
+                id++;
+                user = {
+                    id,
+                    ...user
+                }
+                database.push(user);
+                res.status(201).json({
+                    status: 201,
+                    result: database
+                });
+            } else {
+                res.status(409).json({
+                    status: 409,
+                    result: user.emailAdress + ' is not unique'
+                });
+            }
+        } else {
+            res.status(401).json({
+                status: 401,
+                result: 'Forbidden'
+            });
+        }
+        res.end();
+    },
+    getUserProfile: (req, res) => {
+        if(res.statusCode >= 200 && res.statusCode <= 299) {
+            res.status(200).json({
+                status: 200,
+                result: 'End-point not realised yet'
+            });
+        } else {
+            res.status(401).json({
+                status: 401,
+                result: 'Forbidden'
+            });
+        }
+        res.end();
+    },
+    getUserById: (req, res) => {
+        if(res.statusCode >= 200 && res.statusCode <= 299) {
+            const userId = req.params.userId;
+            if (isNaN(userId)) {
+                res.status(404).json({
+                    status: 404,
+                    result: 'End-point not found'
+                });
+            }
+            let user = database.filter((item) => item.id == userId);
+            if(user.length > 0) {
+                res.status(200).json({
+                    status: 200,
+                    result: user
+                });
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    result: 'None of the users got an id of ' + userId
+                });
+            }
+        } else {
+            res.status(401).json({
+                status: 401,
+                result: 'Forbidden'
+            });
+        }
+        res.end();
+    },
+    updateUserById: (req, res) => {
+        if(res.statusCode >= 200 && res.statusCode <= 299) {
+            const userId = req.params.userId;
+            if (isNaN(userId)) {
+                res.status(404).json({
+                    status: 404,
+                    result: 'End-point not found'
+                });
+            }
+            let user = database.filter((item) => item.id == userId);
+            let id = parseInt(userId);
+            if(user.length > 0) {
+                let updatedUser = req.body;
+                let isUsed = false;
+                database.forEach(item => {
+                    if(item.emailAdress == updatedUser.emailAdress) {
+                        isUsed = true;
+                    }
+                });
+                if(!isUsed) {
+                    updatedUser = {
+                        id,
+                        ...updatedUser
+                    }
+                    database[database.findIndex((item) => item.id == userId)] = updatedUser;
+                    res.status(201).json({
+                        status: 201,
+                        result: database
+                    });
+                } else {
+                    res.status(409).json({
+                        status: 409,
+                        result: updatedUser.emailAdress + ' is not unique'
+                    });
+                }
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    result: 'None of the users got an id of ' + userId
+                });
+            }
+        } else {
+            res.status(401).json({
+                status: 401,
+                result: 'Forbidden'
+            });
+        }
+        res.end();
+    },
+    deleteUserById: (req, res) => {
+        if(res.statusCode >= 200 && res.statusCode <= 299) {
+            const userId = req.params.userId;
+            if (isNaN(userId)) {
+                res.status(404).json({
+                    status: 404,
+                    result: 'End-point not found'
+                });
+            }
+            let user = database.filter((item) => item.id == userId);
+            if(user.length > 0) {
+                database = database.filter((item) => item.id != userId);
+                res.status(201).json({
+                    status: 201,
+                    result: database
+                });
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    result: 'None of the users got an id of ' + userId
+                });
+            }
+        } else {
+            res.status(401).json({
+                status: 401,
+                result: 'Forbidden'
+            });
+        }
+        res.end();
+    }
+};
+
+module.exports = userController;
---
+++ src/routes/user.routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+
+// All /user routes
+router.route('/api/user')
+    // Get all users
+    .get(userController.getAllUsers)
+    // Create an user
+    .post(userController.addUser);
+
+// Get user profile
+router.get('/api/user/profile', userController.getUserProfile);
+
+// All /user/:userId routes
+router.route('/api/user/:userId')
+    // Get specific user on userId
+    .get(userController.getUserById)
+    // Update specific user on userId
+    .put(userController.updateUserById)
+    // Delete specific user on userId
+    .delete(userController.deleteUserById);
+
+module.exports = router;
